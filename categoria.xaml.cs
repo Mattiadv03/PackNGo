17a1f0cd9d962d81c6bc1248d992645b3a5ef7ae
Dalla Valle Mattia <mattiadallavalle03@gmail.com>
2023-08-23 15:53:23 +0200

    aggiunte pagine categoria vacanza

---
+++ categoria.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Pack__n__Go
+{
+    public partial class Categoria : ContentPage
+    {
+        string stagione;
+
+        public Categoria(string stagione)
+        {
+            InitializeComponent();
+
+            this.stagione = stagione;
+        }
+
+        private void MareImageClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Mare(stagione));
+        }
+        
+        private void MontagnaImageClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void LagoImageClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void VisitaImageClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void backClicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
