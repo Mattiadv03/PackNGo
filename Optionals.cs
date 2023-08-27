5bb383b9e3559d0dfe906005924a99b0b85b3b48
Dalla Valle Mattia <mattiadallavalle03@gmail.com>
2023-08-27 12:13:57 +0200

    sostituito file

---
+++ Optionals.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pack__n__Go
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Acne
+    {
+        [JsonProperty("Crema serale")]
+        public int Cremaserale { get; set; }
+
+        [JsonProperty("Crema mattutina")]
+        public int Cremamattutina { get; set; }
+
+        [JsonProperty("Compresse pranzo")]
+        public string Compressepranzo { get; set; }
+    }
+
+    public class Bicicletta
+    {
+        [JsonProperty("Maglia tecnica")]
+        public string Magliatecnica { get; set; }
+
+        [JsonProperty("Pantaloni da bici")]
+        public string Pantalonidabici { get; set; }
+
+        [JsonProperty("Calze tecniche")]
+        public string Calzetecniche { get; set; }
+
+        [JsonProperty("Scarpe da ginnastica")]
+        public int Scarpedaginnastica { get; set; }
+    }
+
+    public class CamminareInMontagna
+    {
+    }
+
+    public class CuraPersonale
+    {
+        public Acne Acne { get; set; }
+    }
+
+    public class Lavoro
+    {
+        public List<string> Computer { get; set; }
+    }
+
+    public class Miscellanous
+    {
+        public List<string> Campeggio { get; set; }
+        public Pioggia Pioggia { get; set; }
+    }
+
+    public class Neve
+    {
+        [JsonProperty("Giacca da neve")]
+        public int Giaccadaneve { get; set; }
+
+        [JsonProperty("Pantaloni da neve")]
+        public int Pantalonidaneve { get; set; }
+
+        [JsonProperty("Calze grosse da neve")]
+        public string Calzegrossedaneve { get; set; }
+
+        [JsonProperty("Scarponcini/Doposcii")]
+        public int ScarponciniDoposcii { get; set; }
+
+        [JsonProperty("Berretto da neve")]
+        public int Berrettodaneve { get; set; }
+
+        [JsonProperty("Calza maglia")]
+        public int Calzamaglia { get; set; }
+    }
+
+    public class Palestra
+    {
+        [JsonProperty("Scarpe da ginnastica")]
+        public int Scarpedaginnastica { get; set; }
+
+        [JsonProperty("Asciugamano in microfibra")]
+        public int Asciugamanoinmicrofibra { get; set; }
+
+        [JsonProperty("Maglia tecnica")]
+        public string Magliatecnica { get; set; }
+
+        [JsonProperty("Pantaloni da ginnastica")]
+        public string Pantalonidaginnastica { get; set; }
+    }
+
+    public class Pioggia
+    {
+        public int Kway { get; set; }
+        public int Ombrello { get; set; }
+    }
+
+    public class PiscinaMare
+    {
+        public string Costume { get; set; }
+        public int Cuffia { get; set; }
+
+        [JsonProperty("Telo mare")]
+        public int Telomare { get; set; }
+
+        [JsonProperty("Borsetta roba onta")]
+        public int Borsettarobaonta { get; set; }
+
+        [JsonProperty("Crema solare faccia")]
+        public int Cremasolarefaccia { get; set; }
+
+        [JsonProperty("Crema solare corpo")]
+        public int Cremasolarecorpo { get; set; }
+    }
+
+
+    public class Sport
+    {
+        [JsonProperty("Piscina/Mare")]
+        public PiscinaMare PiscinaMare { get; set; }
+
+        [JsonProperty("Camminare in montagna")]
+        public CamminareInMontagna Camminareinmontagna { get; set; }
+        public Bicicletta Bicicletta { get; set; }
+        public Palestra Palestra { get; set; }
+    }
+
+    public class Svago
+    {
+        public List<string> Carte { get; set; }
+        public int Libro { get; set; }
+        public Neve Neve { get; set; }
+    }
+    public partial class Optionals
+    {
+        [JsonProperty("Cura personale")]
+        public CuraPersonale Curapersonale { get; set; }
+        public Lavoro Lavoro { get; set; }
+        public Sport Sport { get; set; }
+        public Svago Svago { get; set; }
+        public Miscellanous Miscellanous { get; set; }
+    }
+}
