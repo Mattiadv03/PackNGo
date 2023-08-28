91f8ee2fc4f77d968129356e0be3385e924ab386
Dalla Valle Mattia <mattiadallavalle03@gmail.com>
2023-08-28 19:12:59 +0200

    aggiunto slider durata

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,15 +6,20 @@
         {
             InitializeComponent();
         }
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = Convert.ToInt32(args.NewValue);
+            displayLabel.Text = String.Format("The Slider value is {0}", value);
+        }
 
         private void HotImageClicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new Categoria("estate"));
+            Navigation.PushAsync(new Categoria("estate", Convert.ToInt32(displayLabel.Text)));
         }
 
         private void ColdImageClicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new Categoria("inverno"));
+            Navigation.PushAsync(new Categoria("inverno", Convert.ToInt32(displayLabel.Text)));
         }
     }
 }
